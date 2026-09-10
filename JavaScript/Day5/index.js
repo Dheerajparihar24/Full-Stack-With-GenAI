88b3c8471e1f2323afbe242f2d3626f9434c3fb6
Dheeraj Parihar <dheerajparihar02001@gmail.com>
2026-09-10 23:47:25 +0530

    DSA hasDuplicates problem

--- JavaScript/Day5/index.js
+++ JavaScript/Day5/index.js
@@ -1,25 +1,43 @@
 //Functions
 
 // 1. var leaking out of a block
-let flag = true;
+// let flag = true;
 
-if(flag){
-    var i = 10;
-}
+// if(flag){
+//     var i = 10;
+// }
 
-console.log(i)
+// console.log(i)
 
 //Fix version
 
-if(flag){
-    let varI = 10
-}
-console.log(varI) //Error
+// if(flag){
+//     let varI = 10
+// }
+// console.log(varI)
 
 //2. Accessing a variable outside the function it was declared in
-function fncScope(){
-    var name = "Alex"
-}
+// function fncScope(){
+//     var name = "Alex"
+// }
 
-console.log(name)// error because we are calling function after console.log name;
-fncScope()
+// fncScope()
+// console.log(name)
+
+
+
+// Buggy version:
+function tdzFn(name){
+    console.log(name);
+    let name = "Alex"
+}
+tdzFn("Sam")
+
+// What happens: I'ts show error for let name variable because it's already declared in parameter it's also function part.
+// Why: we can remove the let name variable or change the name, then it's show no bug
+
+// Fixed version:
+function tdzFn(name){
+    console.log(name); // output : "Sam"
+}
+tdzFn("Sam")
